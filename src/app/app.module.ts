ec3a61afde8e50a8d8405930f11e808c31fe1e04
muthumk90 <muthumk90@gmail.com>
2018-06-26 13:45:01 +0530

    Router has been added - router module has been added app module - created header, footer & container components - container used to display dynamic content - router-outlet is used to the container component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { from } from 'rxjs';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContainerComponent } from './container/container.component';
+import { CpaCollegeComponent } from './container/cpa-college/cpa-college.component';
+
+const appRoute :Routes=[
+  {
+    path:'cpa-college', component:CpaCollegeComponent,
+  },
+  {
+    path:'', redirectTo:'/cpa-college', pathMatch:'full'
+  },
+  {
+    path:'**', component:CpaCollegeComponent,
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContainerComponent,
+    CpaCollegeComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+   RouterModule.forRoot(appRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
